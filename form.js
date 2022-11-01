ccd0d15277de36d7c3ae778dacd8b5b7f4b19c3d
sasha52205 <55660001+sasha52205@users.noreply.github.com>
2022-11-01 20:54:40 +0300

    Update form.js

--- form.js
+++ form.js
@@ -4,14 +4,33 @@ function retrieveFormValue(event) {
     event.prevenDefault();
 
     const name = form.querySelector('[name="name"]'),
-        date = form.querySelector('[name="date"]');
+        date = form.querySelector('[name="date"]'),
+        cash = form.querySelector('[name="cash"]'),
+        term = form.querySelector('[name="term"]'),
+        qr = form.querySelector('[name="qr"]'),
+        amount = form.querySelector('[name="amount"]'),
+        stcash = form.querySelector('[name="stcash"]'),
+        fncash = form.querySelector('[name="fncash"]'),
+        incas = form.querySelector('[name="incas"]'),
+        rash = form.querySelector('[name="rash"]'),
+        zp = form.querySelector('[name="zp"]');
 
     const values = {
         name: name.value,
-        date: date.value
+        date: date.value,
+        cash: cash.value,
+        term: term.value,
+        qr: qr.value,
+        amount: amount.value,
+        stcash: stcash.value,
+        fncash: fncash.value,
+        incas: incas.value,
+        rash: rash.value,
+        zp: zp.value
     };
 
     console.log(values)
 }
 
 form.addEventListener('submit', retrieveFormValue);
+
