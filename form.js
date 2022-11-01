90b18fc81b222bfc988bd8cc46e6765eba57c009
sasha52205 <55660001+sasha52205@users.noreply.github.com>
2022-11-01 23:55:14 +0300

    Update form.js

--- form.js
+++ form.js
@@ -49,32 +49,8 @@ function retrieveFormValue(event) {
 
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
-    const name = form.querySelector('[name="name"]'),
-        date = form.querySelector('[name="date"]'),
-        cash = form.querySelector('[name="cash"]'),
-        term = form.querySelector('[name="term"]'),
-        qr = form.querySelector('[name="qr"]'),
-        amount = form.querySelector('[name="amount"]'),
-        stcash = form.querySelector('[name="stcash"]'),
-        fncash = form.querySelector('[name="fncash"]'),
-        incas = form.querySelector('[name="incas"]'),
-        rash = form.querySelector('[name="rash"]'),
-        zp = form.querySelector('[name="zp"]');
-
-    const values = {
-        name: name.value,
-        date: date.value,
-        cash: cash.value,
-        term: term.value,
-        qr: qr.value,
-        amount: amount.value,
-        stcash: stcash.value,
-        fncash: fncash.value,
-        incas: incas.value,
-        rash: rash.value,
-        zp: zp.value
-    };
-	tg.sendData(values);
+	item = "test";
+	tg.sendData(item);
 });
 
 
