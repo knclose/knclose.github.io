772bdecae712d7ef0b954a4d3ce17f505bdb6a63
sasha52205 <55660001+sasha52205@users.noreply.github.com>
2022-11-03 14:45:54 +0300

    Up

--- form.js
+++ form.js
@@ -12,46 +12,6 @@ tg.MainButton.show()
 tg.MainButton.enable()
 
 const form = document.getElementById('form');
-
-function retrieveFormValue(event) {
-    event.preventDefault();
-
-    const name = form.querySelector('[name="name"]'),
-        date = form.querySelector('[name="date"]'),
-        cash = form.querySelector('[name="cash"]'),
-        term = form.querySelector('[name="term"]'),
-        qr = form.querySelector('[name="qr"]'),
-        amount = form.querySelector('[name="amount"]'),
-        stcash = form.querySelector('[name="stcash"]'),
-        fncash = form.querySelector('[name="fncash"]'),
-        incas = form.querySelector('[name="incas"]'),
-        rash = form.querySelector('[name="rash"]'),
-        zp = form.querySelector('[name="zp"]');
-
-    const values = {
-        name: name.value,
-        date: date.value,
-        cash: cash.value,
-        term: term.value,
-        qr: qr.value,
-        amount: amount.value,
-        stcash: stcash.value,
-        fncash: fncash.value,
-        incas: incas.value,
-        rash: rash.value,
-        zp: zp.value
-    };
-
-    console.log(values)
-    window.location.href = 'tnanks.html';
-    // tg.sendData(values)
-}
-
-
-
-form.addEventListener('submit', retrieveFormValue);
-
-
 item = "6";
 const name = form.querySelector('[name="name"]'),
         date = form.querySelector('[name="date"]'),
