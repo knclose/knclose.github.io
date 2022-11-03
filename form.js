a59f1b8619f90d64670f920030d642401d30fd8f
sasha52205 <55660001+sasha52205@users.noreply.github.com>
2022-11-03 20:43:06 +0300

    hjk

--- form.js
+++ form.js
@@ -13,29 +13,13 @@ tg.MainButton.enable()
 
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	const name = document.getElementById('name').value,
-		date = document.getElementById('date').value,
-		cash = document.getElementById('cash').value,
-		term = document.getElementById('term').value,
-		qr = document.getElementById('qr').value,
-		amount = document.getElementById('amount').value,
-		stcash = document.getElementById('stcash').value,
-		fncash = document.getElementById('fncash').value,
-		incas = document.getElementById('incas').value,
-		rash = document.getElementById('rash').value,
-		zp = document.getElementById('zp').value;
+    name = document.getElementById("name").value;
+    date = document.getElementById("date").value;
+    zp = document.getElementById("zp").value;
 
     	const values = {
 		name: name.value,
 		date: date.value,
-		cash: cash.value,
-		term: term.value,
-		qr: qr.value,
-		amount: amount.value,
-		stcash: stcash.value,
-		fncash: fncash.value,
-		incas: incas.value,
-		rash: rash.value,
 		zp: zp.value};
 	
 	alert(JSON.stringify(values));
