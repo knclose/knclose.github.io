bc8efca652ab82a12759f08bb4f4e1cd250524ad
sasha52205 <55660001+sasha52205@users.noreply.github.com>
2022-11-03 05:31:47 +0300

    Update form.js

--- form.js
+++ form.js
@@ -5,7 +5,7 @@ tg.expand();
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#2cab37";
 
-tg.MainButton.text = "Отправить отчет!!!"
+tg.MainButton.text = "Отправить отчет1!"
 tg.MainButton.isVisible = true
 tg.MainButton.isActive = true
 tg.MainButton.show()
@@ -53,33 +53,7 @@ form.addEventListener('submit', retrieveFormValue);
 
 
 item = "6";
-// const name = form.querySelector('[name="name"]'),
-//         date = form.querySelector('[name="date"]'),
-//         cash = form.querySelector('[name="cash"]'),
-//         term = form.querySelector('[name="term"]'),
-//         qr = form.querySelector('[name="qr"]'),
-//         amount = form.querySelector('[name="amount"]'),
-//         stcash = form.querySelector('[name="stcash"]'),
-//         fncash = form.querySelector('[name="fncash"]'),
-//         incas = form.querySelector('[name="incas"]'),
-//         rash = form.querySelector('[name="rash"]'),
-//         zp = form.querySelector('[name="zp"]');
-
-// const values = {
-//         name: name.value,
-//         date: date.value,
-//         cash: cash.value,
-//         term: term.value,
-//         qr: qr.value,
-//         amount: amount.value,
-//         stcash: stcash.value,
-//         fncash: fncash.value,
-//         incas: incas.value,
-//         rash: rash.value,
-//         zp: zp.value};
-
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	const name = form.querySelector('[name="name"]'),
+const name = form.querySelector('[name="name"]'),
         date = form.querySelector('[name="date"]'),
         cash = form.querySelector('[name="cash"]'),
         term = form.querySelector('[name="term"]'),
@@ -91,17 +65,19 @@ Telegram.WebApp.onEvent("mainButtonClicked", function(){
         rash = form.querySelector('[name="rash"]'),
         zp = form.querySelector('[name="zp"]');
 
-	const values = {
-		name: name.value,
-		date: date.value,
-		cash: cash.value,
-		term: term.value,
-		qr: qr.value,
-		amount: amount.value,
-		stcash: stcash.value,
-		fncash: fncash.value,
-		incas: incas.value,
-		rash: rash.value,
-		zp: zp.value};
+const values = {
+        name: name.value,
+        date: date.value,
+        cash: cash.value,
+        term: term.value,
+        qr: qr.value,
+        amount: amount.value,
+        stcash: stcash.value,
+        fncash: fncash.value,
+        incas: incas.value,
+        rash: rash.value,
+        zp: zp.value};
+
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	alert(values);
 });
